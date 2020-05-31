67bdd409f403c5d8c61f6508710398397965acaa
Artur Leshko <61659047+Artur-Leshko@users.noreply.github.com>
2020-05-31 16:07:29 +0300

    Update script.js

--- lesson_5/task_1/script.js
+++ lesson_5/task_1/script.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function addLi(event) {
+        let key = event.key;
+        let ul = document.querySelector('ul');
+        let li = document.createElement('li');
 
+        li.textContent = key;
+        ul.appendChild(li);
+    }
+
+    let input = document.querySelector('input');
+
+    input.addEventListener('keyup', addLi);
+});
