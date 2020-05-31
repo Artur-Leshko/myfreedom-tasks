2ad4a543a5122613a700b56e96e857851a633473
Artur Leshko <61659047+Artur-Leshko@users.noreply.github.com>
2020-05-31 19:29:23 +0300

    Update script.js

--- lesson_5/task_4/script.js
+++ lesson_5/task_4/script.js
@@ -1,1 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function calc(event) {
+        event.preventDefault();
 
+        let firstOperand = document.querySelector('.first__operand').value;
+        let secondOperand = document.querySelector('.second__operand').value;
+        let sign = document.querySelector('select').value;
+        let findDiv = document.querySelector('div');
+        let result = String(firstOperand) + String(sign) + String(secondOperand);
+
+        result = eval(result);
+
+        findDiv.textContent = result;
+    }
+
+    let findForm = document.querySelector('form');
+
+    findForm.addEventListener('submit', calc);
+});
