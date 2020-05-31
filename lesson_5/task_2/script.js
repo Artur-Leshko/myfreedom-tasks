140e2969c6ab5c03f252a9409f4ab1fb4b9d9b48
Artur Leshko <61659047+Artur-Leshko@users.noreply.github.com>
2020-05-31 16:42:47 +0300

    Update script.js

--- lesson_5/task_2/script.js
+++ lesson_5/task_2/script.js
@@ -1,1 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function inputAction(event) {
+        let inputValue = document.querySelector('input').value;
+        let findDiv = document.querySelector('div');
+        let addP = document.createElement('p');
 
+        console.log(inputValue);
+
+        addP.textContent = `Клиент набирает: ${inputValue}`;
+        findDiv.appendChild(addP);
+    }
+
+    let input = document.querySelector('input');
+
+    input.addEventListener('keyup', inputAction);
+});
