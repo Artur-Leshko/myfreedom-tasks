69305b3be15a1bf5bf280b2fe07b5458bd205ec8
Artur Leshko <61659047+Artur-Leshko@users.noreply.github.com>
2020-06-10 12:32:34 +0300

    Update script.js

--- lesson_8/task_3/script.js
+++ lesson_8/task_3/script.js
@@ -1,1 +1,24 @@
+function toWeirdCase(string){
+  let words = string.split(' ');
+  
+  for (let i = 0; i < words.length; i++) {
+    words[i] = toSomeCase(words[i]);
+  }
+  
+  string = words.join(' ');
+  
+  return string;
+}
 
+function toSomeCase(word) {
+  let newWord = '';
+  for (let i = 0; i < word.length; i++) {
+    if (i % 2 == 0) {
+      newWord += word[i].toUpperCase();
+    } else {
+      newWord += word[i].toLowerCase();
+    }
+  }
+  
+  return newWord;
+}
