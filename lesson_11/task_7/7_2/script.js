00f943dabfc5f6f2029bf983b25b594b195189b4
Artur Leshko <61659047+Artur-Leshko@users.noreply.github.com>
2020-06-20 23:57:54 +0300

    Update script.js

--- lesson_11/task_7/7_2/script.js
+++ lesson_11/task_7/7_2/script.js
@@ -1,1 +1,16 @@
-
+function maskify(cc) {
+  if (cc.length <= 4) return cc;
+  else {
+    let newString = '';
+    
+    for (let i = 0; i < cc.length - 4; i++){
+      newString += '#';
+    }
+    
+    for (let i = cc.length - 4; i < cc.length; i++) {
+      newString += cc[i];
+    }
+    
+    return newString;
+  }
+}
