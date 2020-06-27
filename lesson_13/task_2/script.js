74fb268553fc9801d2e5834ee124f0d759112481
Artur Leshko <61659047+Artur-Leshko@users.noreply.github.com>
2020-06-27 19:39:10 +0300

    Update script.js

--- lesson_13/task_2/script.js
+++ lesson_13/task_2/script.js
@@ -1,1 +1,49 @@
+class Calc {
+    constructor(initNumber) {
+        this.value = initNumber;
+    }
 
+    plus(addendum) {
+        this.value = this.value + addendum;
+        document.querySelector('.result').value = this.value;
+    }
+
+    minus(addendum) {
+        this.value = this.value - addendum;
+        document.querySelector('.result').value = this.value;
+    }
+
+    multiply(addendum) {
+        this.value = this.value * addendum;
+        document.querySelector('.result').value = this.value;
+    }
+
+    divide(addendum) {
+        this.value = this.value / addendum;
+        document.querySelector('.result').value = this.value;
+    }
+}
+
+const calc = new Calc(10);
+let enter = document.querySelector('.enter');
+document.querySelector('.result').value = 10;
+
+document.querySelector('.plus').addEventListener('click', function(event) {
+    calc.plus(Number(enter.value));
+    enter.value = '';
+})
+
+document.querySelector('.minus').addEventListener('click', function(event) {
+    calc.minus(Number(enter.value));
+    enter.value = '';
+})
+
+document.querySelector('.multiply').addEventListener('click', function(event) {
+    calc.multiply(Number(enter.value));
+    enter.value = '';
+})
+
+document.querySelector('.divide').addEventListener('click', function(event) {
+    calc.divide(Number(enter.value));
+    enter.value = '';
+})
