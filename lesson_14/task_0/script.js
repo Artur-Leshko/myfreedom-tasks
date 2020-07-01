9630da8592048bf016fedda0bba5f3d3f9e4cc3d
Artur Leshko <61659047+Artur-Leshko@users.noreply.github.com>
2020-07-01 13:25:39 +0300

    Update script.js

--- lesson_14/task_0/script.js
+++ lesson_14/task_0/script.js
@@ -1,1 +1,48 @@
+let button = document.querySelector('button');
+
+// button.addEventListener('click', async function(event) {
+//     try {
+//         let requset = await fetch(`https://todoappexamplejs.herokuapp.com/items/1178`, {
+//             method: 'PUT',
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({title: 'lol'})
+//         })
+//         requset = await requset.json();
+
+//         if(requset.title === undefined) {
+//             let err = new Error('Ошибка!');
+//             throw err;
+//         }
+        
+//         console.log('Успех!');
+//     } catch(e) {
+//         console.log(e);
+//     } finally {
+//         console.log('Конец!');
+//     }
+// });
+
+button.addEventListener('click', function(event) {
+    let requset = fetch('https://todoappexamplejs.herokuapp.com/items/115', {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title: 'lol'})
+    })
+
+    requset.then(response => {
+        if(!response.ok) {
+            throw response;
+        }
+        return response.json();
+    }).then(data => console.log('Успех!')).catch(err => {
+        console.log('Ошибка!');
+        err.text().then(errorMessage => console.log(errorMessage));
+    })
+})
 
