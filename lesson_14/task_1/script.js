30f049ecad187089f60a9fd97ccfa6b3f9dc9275
Artur Leshko <61659047+Artur-Leshko@users.noreply.github.com>
2020-07-01 14:31:54 +0300

    Update script.js

--- lesson_14/task_1/script.js
+++ lesson_14/task_1/script.js
@@ -1,1 +1,16 @@
+let names = ['iliakan', 'remy', 'jeresig'];
+let promises = names.map(name => fetch(`https://api.github.com/users/${name}`));
 
+Promise.all(promises).then(responses => {
+    for(let i = 0; i < 2; i++) {
+        for(let element of responses) {
+            console.log(element);
+        }
+    }
+}).then(() => {
+    for(let i = 0; i < 2; i++) {
+        for(let element of names) {
+            console.log(element);
+        }
+    }
+})
