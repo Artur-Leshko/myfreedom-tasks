25c076e77f192d7ebc4c28002ddfeee5587fc1c2
Alexander Ryhlitsky <alexander.rykhlitskiy@gmail.com>
2020-07-15 20:19:34 +0300

    Fix events handling

--- lesson_18/18_1/script.js
+++ lesson_18/18_1/script.js
@@ -1,4 +1,4 @@
-class AddSticker {
+class StickerCreator {
     constructor(value) {
         this.value = value;
     }
@@ -42,22 +42,22 @@ class AddSticker {
 
     addSticker() {
         let sticker = this._createSticker();
-        let temprorySticker;
-        
+        var deltaX;
+        var deltaY;
+
+        let mousemove = function(event) {
+            sticker.style.top = `${event.clientY - deltaY}px`;
+            sticker.style.left = `${event.clientX - deltaX}px`;
+        }
+
         sticker.addEventListener('mousedown', function(event) {
-            temprorySticker = sticker;
+            deltaY = event.clientY - sticker.offsetTop;
+            deltaX = event.clientX - sticker.offsetLeft;
+            window.addEventListener('mousemove', mousemove)
         });
 
-        sticker.addEventListener('mousemove', function(event) {
-            if(temprorySticker){
-                sticker.style.top = `${event.clientX}px`;
-                sticker.style.left = `${event.clientY}px`;
-            }
-        })
-
         sticker.addEventListener('mouseup', function(event) {
-            sticker = temprorySticker;
-            temprorySticker = null;
+            window.removeEventListener('mousemove', mousemove)
         });
 
         document.body.appendChild(sticker);
@@ -68,7 +68,7 @@ let form = document.querySelector('form');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    let newSticker = new AddSticker(document.querySelector('.inputTask').value);
+    let newSticker = new StickerCreator(document.querySelector('.inputTask').value);
     document.querySelector('.inputTask').value = '';
     newSticker.addSticker();
 });
