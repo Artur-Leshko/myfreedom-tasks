805de8e622c887e7394219be11ce1abc1d21c48b
Artur Leshko <fire.ghost.01@mail.ru>
2020-07-15 14:34:59 +0300

    add timer and reset of the game

--- lesson_18/18_2/script.js
+++ lesson_18/18_2/script.js
@@ -1,10 +1,38 @@
 let duck = document.querySelector('.duck');
+let time = document.querySelector('.time');
 let start = document.querySelector('.start');
+let end_game = document.querySelector('.end_game');
+let restart = document.querySelector('.restart');
+let scaleX = -1;
 let trigger = false;
 let counter = 0;
+let seconds = 60;
+
+timer = setInterval(function() {
+
+    if(seconds <= 0) {
+        trigger = false;
+        time.textContent = '0';
+        document.querySelector('.end_game__status').textContent = 'Поражение!';
+        document.querySelector('.end_game').style.display = 'flex';
+    } else {
+        if(counter === 5) {
+            trigger = false;
+            time.textContent = '0';
+            document.querySelector('.end_game__status').textContent = 'Победа!';
+            document.querySelector('.end_game').style.display = 'flex';
+            return 0;
+        }
+
+        time.textContent = `${seconds}`;
+    }
+
+    seconds--;
+}, 1000);
 
 start.addEventListener('click', function(event) {
     trigger = true;
+    timer;
     event.target.remove();
 });
 
@@ -12,10 +40,6 @@ duck.addEventListener('mousedown', function(event) {
     if(trigger) {
         counter++;
         document.querySelector('.counter').textContent = counter;
-
-        if(counter === 5) {
-
-        }
     }
 });
 
@@ -29,7 +53,18 @@ duck.addEventListener('mousemove', function(event) {
         let newWidthCoord = Math.random() * (clientWidth - duckWidth);
         let newHeightCoord = Math.random() * (clientHeight - duckHeight);
         
+        scaleX = -scaleX;
         duck.style.top = `${newHeightCoord}px`;
         duck.style.left = `${newWidthCoord}px`;
+        duck.style.transform = `scaleX(${scaleX})`;
     }
+});
+
+restart.addEventListener('click', function(event) {
+    trigger = true;
+    seconds = 60;
+    timer;
+    document.querySelector('.end_game').style.display = 'none';
+    counter = 0;
+    document.querySelector('.counter').textContent = counter;
 });
