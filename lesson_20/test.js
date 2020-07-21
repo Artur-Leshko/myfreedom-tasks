80bf4226411e22cd3496835722eaa1f28926c2ec
Artur Leshko <fire.ghost.01@mail.ru>
2020-07-22 02:20:27 +0300

    fulfill first task

--- lesson_20/test.js
+++ lesson_20/test.js
@@ -1,7 +1,16 @@
 describe("map", function() {
 
-    it("возводит каждый элемент массива в квадрат", function() {
-      assert.equal(map([1, 2, 3], sqr), [1, 4, 9]);
-    });
+    // it("возводит каждый элемент массива в квадрат", function() {
+    // //   assert.deepEqual(map([1, 2, 3], sqr), [1, 4, 9]);
 
+    //     const array = [1,2,3];
+    //     const expectedArray = array.map(item => item ** 2)
+    //     assert.deepEqual(map(array, sqr), expectedArray);
+    // });
+
+    for(let i = 0; i < 10; i += 3) {
+        it("возводит каждый элемент массива в квадрат", function() {
+            assert.deepEqual(map([ i, i + 1, i + 2 ], sqr), [ i ** 2, (i + 1) ** 2, (i + 2) ** 2 ]);
+        });
+    }
 });
